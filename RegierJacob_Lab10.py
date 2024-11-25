8b4636c3498121a46226d8d46320637efd693cd0
Jacob <jregier8@uwo.ca>
2024-11-24 20:59:20 -0500

    Added a function create_tridiagonal_matrix to generate tridiagonal matrices. This function takes matrix size and values for the diagonal, upper diagonal, and lower diagonal elements as inputs. Included a basic example to verify its functionality.

--- RegierJacob_Lab10.py
+++ RegierJacob_Lab10.py
@@ -1,2 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def create_tridiagonal_matrix(size, below_diag, diag, above_diag):
+    matrix = np.zeros((size, size))
+    np.fill_diagonal(matrix, diag)
+    np.fill_diagonal(matrix[1:], below_diag)
+    np.fill_diagonal(matrix[:, 1:], above_diag)
+    return matrix
+
+size = 5
+A = create_tridiagonal_matrix(size, below_diag=3, diag=1, above_diag=5)
+
